217657569e5d73719ba10b834ba22d48c0deeeea
slin <345978511@qq.com>
2025-01-26 22:12:20 +0800

    echarts 配置项数据

--- src/api/mockServerData/menudata.js
+++ src/api/mockServerData/menudata.js
@@ -1,4 +1,4 @@
-// js 数据
+``` js 数据
 menuData: [
     {
       path: "/",
@@ -42,3 +42,56 @@ menuData: [
       ]
     }
   ]
+```js 数据
+   //折线图和柱状图的配置项
+   let xOptions = reactive({
+    //图例文字颜色
+    textStyle: {
+      color: "#333"
+    },
+    grid: {
+      left: "20%"
+    },
+    tooltip: {
+      trigger: "axis"
+    },
+    xAxis: {
+      type: "category",
+      data: [],
+      axisLine: {
+        lineStyle: {
+          color: "#17b3a3"
+        }
+      },
+      axisLabel: {
+        interal: 0,
+        color: "#333"
+      }
+    },
+    yAxis: {
+      type: "value",
+      axisLine: {
+        lineStyle: {
+          color: "#17b3a3"
+        }
+      }
+    },
+    color: ["#2ec7c9", "#b6a2de", "#5ab1ef", "#ffb980", "#d87a80", "#8d98b3"],
+    series: []
+  });
+  // 饼状图的配置项
+  let pieOptions = reactive({
+      tooltip:{
+          trigger:"item"
+      },
+      color:[
+          "#0f78f4",
+          "#dd536b",
+          "#9462e5",
+          "#a6a6a6",
+          "#e1bb22",
+          "#39c362",
+          "#3ed1ef"
+      ],
+      series:[]
+  });
